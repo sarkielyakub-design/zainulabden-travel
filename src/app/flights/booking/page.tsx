8ed24b4b9fb4e306ba5f35bdb7021d0acbf72431
your name <you@email.com>
2026-06-11 20:47:58 +0100

    page

---
+++ src/app/flights/booking/page.tsx
@@ -0,0 +1,9 @@
+export default function FlightBookingPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Flight Booking Page
+      </h1>
+    </div>
+  );
+}
